7d16741e1ca3b5b52466fecdb00e7c4f78c243cb
berchapman <berchapman@gmail.com>
2013-10-10 15:16:57 -0700

    pcs assignment 2

---
+++ pcshw2.js
@@ -0,0 +1,8 @@
+var myString = "Ber Chapman"
+var output = ""
+
+for (var i = myString.length - 1; i > -1; --i) {
+	if (myString[i] != "a" && myString[i] != "e" && myString != "i" && myString != "o" && myString != "u" && myString != "y") {
+		output = output + myString[i]
+	}
+}
